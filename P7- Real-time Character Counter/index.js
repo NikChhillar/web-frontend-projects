ed9f1046a0b59f349b49eb8bc6680e2bb074d702
Nikhil <nikhilchhillar29@gmail.com>
2023-01-17 08:48:01 +0530

    final commit

---
+++ P7- Real-time Character Counter/index.js
@@ -0,0 +1,13 @@
+const textareaE = document.getElementById("textarea");
+const totalC = document.getElementById("total-counter");
+const remainingC = document.getElementById("remaining-counter");
+
+textareaE.addEventListener("keyup", () => {
+    updateCount();
+});
+updateCount();
+function updateCount() {
+    totalC.innerText = textareaE.value.length;
+
+    remainingC.innerText = textareaE.getAttribute("maxLength") - textareaE.value.length;
+}
