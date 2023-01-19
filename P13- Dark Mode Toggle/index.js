523eb8e77f6fe7c4f436f56874339f808b7ea0a8
Nikhil <nikhilchhillar29@gmail.com>
2023-01-19 11:14:38 +0530

    changed toggle color

--- P13- Dark Mode Toggle/index.js
+++ P13- Dark Mode Toggle/index.js
@@ -3,6 +3,12 @@ const bodyE = document.querySelector("body");
 
 function updateBody() {
     if (inputE.checked) {
-
+        bodyE.style.background = "black";
+    } else {
+        bodyE.style.background = "white";
     }
 }
+
+inputE.addEventListener("input", () => {
+    updateBody();
+})
